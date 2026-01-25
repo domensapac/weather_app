56b41015167d32181ef127f57ef02a1efe77ef20
domensapac <domen.sapac10@gmail.com>
2026-01-25 21:29:23 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="script.js"></script>
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="col-6">
+        <h3 id="placeName"></h3>
+        <h3 id="municipality">a</h3>
+        <h3 id="elevation"></h3>
+        <h3 id="latitude"></h3>
+        <h3 id="longitude"></h3>
+    </div>
+    <div class="col-6">
+        <h3 id="temperature">
+        <h3 id="precProbability">
+    </div>
+</div>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,62 @@
+var placeNameTxt; 
+var municipalityTxt; 
+var elevationTxt; 
+var latitudeTxt;
+var longitudeTxt; 
+var temperatureTxt; 
+var precProbabilityTxt; 
+
+window.onload = () =>{
+    placeNameTxt = document.getElementById("placeName"); 
+    municipalityTxt = document.getElementById("municipality"); 
+    elevationTxt = document.getElementById("elevation");
+    latitudeTxt = document.getElementById("latitude");
+    longitudeTxt = document.getElementById("longitude");
+    temperatureTxt = document.getElementById("temperature");
+    precProbabilityTxt = document.getElementById("precProbability");
+    getCoords(); 
+}; 
+
+async function getCoords(){ //dobimo koordinate kraja
+    const name = 'Bodonci'; 
+    const apiUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${name}&count=1&language=en&format=json`
+    const response = await fetch(apiUrl);
+    let data = await response.json(); 
+    updateText(
+        data.results[0].name,
+        data.results[0].admin1,
+        data.results[0].elevation,
+        data.results[0].latitude,
+        data.results[0].longitude,
+    )
+
+    getData(
+        data.results[0].latitude,
+        data.results[0].longitude,
+    )
+}
+
+async function getData(latitude, longitude){ //dobimo podatke za kraj
+    const latVal = latitude; 
+    const lonVal = longitude;
+    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latVal}&longitude=${lonVal}&hourly=temperature_2m,precipitation_probability&timezone=Europe%2FBerlin&forecast_days=3`; 
+    const response = await fetch(apiUrl); 
+    let data = await response.json(); 
+    outputData(data); 
+}
+
+
+function updateText(placeName, municipality, elevation, latitude, longitude){
+    placeNameTxt.textContent = "Kraj: " + placeName; 
+    municipalityTxt.textContent = "Občina: " + municipality; 
+    elevationTxt.textContent = "Nadmorska: " + elevation; 
+    latitudeTxt.textContent = "Latitude: " + latitude; 
+    longitudeTxt.textContent = "Longitude: " + longitude; 
+}
+
+function outputData(data){
+    console.log(data); 
+    array.forEach(element => {
+        
+    });
+}
