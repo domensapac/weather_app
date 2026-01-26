7e2e0c63d10473f51c04af3443f92d0be92dca8d
dddomennn <domen.sapac420@gmail.com>
2026-01-26 16:31:40 +0100

    Logic

--- index.php
+++ index.php
@@ -5,28 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<div class="container-fluid">
-    <div class="col-6">
-        <h3 id="placeName"></h3>
-        <h3 id="municipality"></h3>
-        <h3 id="elevation"></h3>
-        <h3 id="latitude"></h3>
-        <h3 id="longitude"></h3>
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            
+        </div>
+        <div class="col-6">
+            <h3 id="placeName"></h3>
+            <h3 id="municipality"></h3>
+            <h3 id="elevation"></h3>
+            <h3 id="latitude"></h3>
+            <h3 id="longitude"></h3>
+        </div>
+        <div class="col-3">
+        </div>
     </div>
-    <div class="col-6">
-        <h3 id="temperature">
-        <h3 id="precProbability">
+
+
+    <div class="row justify-content-md-center">
+        <div class="col col-lg-2">
+        </div>
+        <div id="contentArea" class="col-md-auto">
+            <!--
+            <div class="cardElement">
+                <div class="dayText">
+                    <span>Ponedeljek</span>
+                </div>
+                <div class="tempText">
+                    <span>3°C</span>
+                </div>
+            </div>
+        -->
+        </div>
+        <div class="col col-lg-2">
+        </div>
     </div>
 </div>
 
 
 
 
-
+<script src="script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -3,8 +3,7 @@ var municipalityTxt;
 var elevationTxt; 
 var latitudeTxt;
 var longitudeTxt; 
-var temperatureTxt; 
-var precProbabilityTxt; 
+var contentArea; 
 
 window.onload = () =>{
     placeNameTxt = document.getElementById("placeName"); 
@@ -12,8 +11,7 @@ window.onload = () =>{
     elevationTxt = document.getElementById("elevation");
     latitudeTxt = document.getElementById("latitude");
     longitudeTxt = document.getElementById("longitude");
-    temperatureTxt = document.getElementById("temperature");
-    precProbabilityTxt = document.getElementById("precProbability");
+    contentArea = document.getElementById("contentArea"); 
     getCoords(); 
 }; 
 
@@ -42,7 +40,8 @@ async function getData(latitude, longitude){ //dobimo podatke za kraj
     const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latVal}&longitude=${lonVal}&hourly=temperature_2m,precipitation_probability&timezone=Europe%2FBerlin&forecast_days=3`; 
     const response = await fetch(apiUrl); 
     let data = await response.json(); 
-    outputData(data); 
+    outputDataConsole(data); 
+    displayData(data);
 }
 
 
@@ -54,14 +53,36 @@ function updateText(placeName, municipality, elevation, latitude, longitude){
     longitudeTxt.textContent = "Longitude: " + longitude; 
 }
 
-function outputData(data){
-    console.log(data); 
-    for(var i=0; i< data.hourly.; i++){ // !
-        console.log("a");
+function outputDataConsole(data){
+    const dataHourly = data.hourly; 
+    const currDate = new Date(); 
+    for(var i=0; i< dataHourly.temperature_2m.length ; i++){ 
+        const forecastDate = new Date(dataHourly.time[i]); 
+        if (forecastDate >= currDate) {
+            //console.log(dataHourly.time[i] + " : " + dataHourly.temperature_2m[i]); 
+        }
     }
-    /*
-    Object.values(data.temperature_2m).forEach(element => {
-        console.log(element); 
-    });
-    */
 }
+
+function displayData(data){
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const dataHourly = data.hourly; 
+    var arrDays = new Set();
+    console.log(data); 
+
+    for(var i=0; i< dataHourly.time.length ; i++){
+        let dateTime = dataHourly.time[i].split("T")[0] ; 
+        arrDays.add(dateTime); 
+    }
+    const uniqueDays = Array.from(arrDays); // TEGA UPORABLJAS 
+    
+
+    for(var i=0; i<3; i++){
+
+        var element = document.createElement("div"); 
+        element.innerHTML = `<div class='dayText'> ${uniqueDays[i]} </div> <div class='tempText'> ${i}${data.hourly_units.temperature_2m}</div>`; 
+        element.setAttribute("class", "cardElement"); 
+        contentArea.appendChild(element); 
+    }
+}
+
